e87a520b48b25a08fd47ef6a0257d248863c61af
Loïc Torres <loic.torresg@gmail.com>
2021-08-20 13:56:16 +0200

    change(deploy): ./_app.jsx: changed getInitial by getStaticProps

--- components/smallCover.jsx
+++ components/smallCover.jsx
@@ -4,13 +4,13 @@ import NextImage from "./coverImage"
 const SmallCover = ({ img, title }) => {
   return (
     <div className="uk-position-relative uk-height-medium uk-background-cover skew uk-height-correction uk-box-shadow-small">
-      <NextImage
+      {image && <NextImage
         priority
         image={img}
         blurDataURL={`/${img.name}`}
         placeholder={"blur"}
         uk-scrollspy="cls: uk-animation-kenburns; repeat: true"
-      />
+      />}
       <h2 className="uk-light uk-position-center uk-text-large uk-margin-remove">
         {title}
       </h2>
