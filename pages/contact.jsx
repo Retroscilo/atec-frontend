b49dedc7d585cbdde38b1e9b827437c8667b0704
Loïc Torres <loic.torresg@gmail.com>
2021-08-30 12:46:20 +0200

    page(contact)

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -27,7 +27,7 @@ const Contact = ({ contact }) => {
             <a className="text-primary" href="mailto:marc@torresrun.fr"><span className="uk-margin-right uk-margin-top" uk-icon="icon: mail; ratio: 1.5" />marc@torresrun.fr</a>
           </span>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2257.427098710808!2d55.37256406252848!3d-21.255903704049295!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2182a26ebb563629%3A0xe0c47de6a599f917!2s22-19%20Lot.%20les%20Coteaux%20de%20l&#39;Avenir%2C%20Etang-Sal%C3%A9%20les%20Hauts%2C%20R%C3%A9union!5e0!3m2!1sfr!2sfr!4v1630318411402!5m2!1sfr!2sfr"
+            src={"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2257.427098710808!2d55.37256406252848!3d-21.255903704049295!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2182a26ebb563629%3A0xe0c47de6a599f917!2s22-19%20Lot.%20les%20Coteaux%20de%20l&#39;Avenir%2C%20Etang-Sal%C3%A9%20les%20Hauts%2C%20R%C3%A9union!5e0!3m2!1sfr!2sfr!4v1630318411402!5m2!1sfr!2sfr"}
             width="400"
             height="250"
             style={{ border: 0 }}
